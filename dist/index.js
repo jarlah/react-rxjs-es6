cfeccc62dc7d2162a784817dcf2659c586747801
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-09-06 18:57:26 +0200

    fix: add flow to the flow

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,43 @@
-export const RxProvide = require('./RxProvide');
-export const RxStore = require('./RxStore');
-export const RxInject = require('./RxInject');
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _RxInject = require('./RxInject');
+
+Object.defineProperty(exports, 'RxInject', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_RxInject).default;
+  }
+});
+
+var _RxContainer = require('./RxContainer');
+
+Object.defineProperty(exports, 'RxContainer', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_RxContainer).default;
+  }
+});
+
+var _RxStore = require('./RxStore');
+
+Object.defineProperty(exports, 'RxStore', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_RxStore).default;
+  }
+});
+
+var _RxProvide = require('./RxProvide');
+
+Object.defineProperty(exports, 'RxProvide', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_RxProvide).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as RxInject } from './RxInject';
+export { default as RxContainer } from './RxContainer';
+export { default as RxStore } from './RxStore';
+export { default as RxProvide } from './RxProvide';
